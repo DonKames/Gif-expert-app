6be8a59511f38e1fc2e0d56633459b8014703a33
Camilo Santander <demon_camilo@hotmail.com>
2022-01-16 15:35:07 -0300

    First Commit Curso

---
+++ src/tests/GifExpertApp.test.js
@@ -0,0 +1,17 @@
+import { shallow } from "enzyme";
+import GifExpertApp from "../GifExpertApp";
+//import AddCategory from "../components/AddCategory";
+
+describe('Pruebas <GifExpertApp />', () => {
+    let wrapper = shallow(<GifExpertApp />);
+
+    beforeEach(() => {
+        wrapper = shallow(<GifExpertApp />);
+    });
+
+    test('Debe cargar el componente correctamente ', () => {
+        console.log(wrapper.html());
+        expect(wrapper).toMatchSnapshot();
+    })
+    
+})
